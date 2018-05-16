37c631c2e4e03d38d0c3e58d80cc9aa894a555a1
Nicholas Codalata <nicholascodalata@usicllc.com>
2018-05-16 11:09:22 -0400

    Added Appcenter.

--- DigCheckPro/App.cs
+++ DigCheckPro/App.cs
@@ -2,6 +2,10 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace DigCheckPro
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -12,6 +16,12 @@ namespace DigCheckPro
             var homePage = new NavigationPage(new HomePage()) { Title = "Home Page" };
 
             MainPage = homePage;
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            AppCenter.Start("android=b531b684-e9d2-415d-8cdc-608de4f99893;", typeof(Analytics), typeof(Crashes));
         }
     }
 }
